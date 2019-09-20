770e4226af67f90af911cdc8f5175e1d50f28cb7
greyj <evilover@163.com>
2019-09-20 08:29:55 +0800

    9.20-zong

--- src/components/search/comprehensive/index.js
+++ src/components/search/comprehensive/index.js
@@ -30,7 +30,6 @@ class Comprehensive extends Component{
                         marginLeft:"0.4rem",
                         color:"lightgreen"
                     }}
-
                     >视频</h3>
                     <Video></Video>
                     <h3 style={{
@@ -39,12 +38,6 @@ class Comprehensive extends Component{
                     }}
                     >歌手</h3>
                     <Singer></Singer>
-
-
-
-
-
-
                 </ul>
             </>
         )
