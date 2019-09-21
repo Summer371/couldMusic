8cecaf51ed5c9ddf621509a2add6a7e0d44e7fe0
hetenfei <1029531444@qq.com>
2019-09-21 19:14:43 +0800

    完善电台音频进度条跟随效果

--- src/views/player/FF.js
+++ src/views/player/FF.js
@@ -21,7 +21,10 @@ class FF extends React.Component {
             }],
             urlList: [],
             timer: null,
-            num:0
+            num:0,//每秒增加时间
+            play:true,//判断歌曲点击播放
+            jdw:0,//歌曲每秒增加长度
+            ssjj:0//歌曲总时长
         }
     }
 
@@ -46,17 +49,25 @@ class FF extends React.Component {
                 <div className={"playerBodyy"}>
                     {songsUrl.map((v, i) => (
                         <div key={i}>
-                            <audio src={v.url} controls={"controls"} preload={"preload"} id={"audio"}
+                            <audio src={v.url}  preload={"preload"} id={"audio"}
                                    autoplay={"autoplay"}></audio>
                         </div>
                     ))}
-
+                    <div className={"progressmusic"}  >
+                        {/*<span>{this.state.num}</span>*/}
+                        <span >{this.$filter.songTime(this.state.num)}</span>
+                        <h3 className={"musicjd"} >
+                            <span className={"paowa"}></span>
+                            <i className={"jzcd"}></i>
+                        </h3>
+                       <span>{this.$filter.FFtime(list[this.state.index].mainSong.lMusic.playTime)}</span>
+                    </div>
                     <div className={"playerBottony"}>
                         <i className={"iconfont icon-xihuan-kongpt"}></i>
                         <i className={"iconfont icon-shangyiqu"} onClick={this.clickZuo.bind(this)}>
                         </i>
                         <span id={"off"} className={"paly"}>
-                            <i className={"zbhh iconfont icon-bofang"} onClick={this.clickbf.bind(this)}>
+                            <i className={this.state.play?"iconfont icon-bofang":"iconfont icon-zanting"} onClick={this.clickbf.bind(this)}>
                             </i>
                         </span>
                         <i className={"iconfont icon-xiayiqu "} onClick={this.clickYou.bind(this)}></i>
@@ -67,28 +78,45 @@ class FF extends React.Component {
         )
     }
 
+    jdw(){
+        var minutes = parseInt((this.state.yyjd % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = (this.state.yyjd % (1000 * 60)) / 1000;
+        let ss=minutes+" : "+seconds
+        if(this.state.yyjd<60){
+            return "0 : "+this.state.yyjd
+        }else{
+            return ss
+        }
+    }
     clickbf() {
         var audio = document.getElementById("audio");
         var musiIocn = document.querySelector(".rotateimg img");
         var off = document.getElementById("off");
         let sj=this.state.num;//获取当前音频时长
         if (off.className == "paly") {  //如果当前播放
+            this.setState({
+                play:false
+            })
             audio.pause(); //停止（暂停）
             off.className = "stop"; //暂停
-            // musiIocn.src = "img/music130.png";  //暂停图片
             musiIocn.className = " "; //取消图片360旋转CSS3动画
             clearInterval(this.state.timer)//关闭音频定时器
         } else if (off.className == "stop") {  //如果当前暂停
+            this.setState({
+                play:true
+            })
             audio.play();  //开始播放
             off.className = "paly";  //开始播放
-            // musiIocn.src = "img/music131.png"; //播放图片
             musiIocn.className = "myimg";  //追加图片360旋转CSS3动画
             this.timerMusic(sj)//开启音频定时器
             sj=0;
         }
     }
-
     clickZuo(e) {
+        let paowu=document.querySelector(".paowa");//小黑点
+        let jzcd=document.querySelector(".jzcd");//进度条颜色
+        jzcd.style.width=0//进度为0
+        paowu.style.left=0//让偏移量重新为0
         clearInterval(this.state.timer)//关闭音频定时器
         this.setState({
             index: --this.props.location.state.index,
@@ -99,6 +127,10 @@ class FF extends React.Component {
     }
 
     clickYou(e) {
+        let paowu=document.querySelector(".paowa");//小黑点
+        let jzcd=document.querySelector(".jzcd");//进度条颜色
+        jzcd.style.width=0//进度为0
+        paowu.style.left=0//让偏移量重新为0
         clearInterval(this.state.timer)//关闭音频定时器
         this.setState({
             index: ++this.props.location.state.index,
@@ -120,19 +152,35 @@ class FF extends React.Component {
     timerMusic(timerSj=0){//控制音频
         var audio = document.getElementById("audio");//获取音频
         let ssj = 0;
-        audio.oncanplay = function () { //获取音乐时长
+        let paowu=document.querySelector(".paowa");//小黑点
+        let musicjd=document.querySelector(".musicjd");//进度条
+        let jzcd=document.querySelector(".jzcd");//进度条颜色
+        let widthwa=musicjd.offsetWidth
+        audio.oncanplay = ()=>{ //获取音乐时长
             console.log(audio.duration)
-            ssj = parseInt(audio.duration)
+            ssj = Number(parseInt(audio.duration))
+            this.setState({
+                ssjj:Number(parseInt(audio.duration)),
+                jdw:paowu.offsetLeft+(widthwa/ssj)
+            })
         }
+        // paowu.offsetLeft+(ssj/musicjd.offsetWidth)
         this.state.timer = setInterval(() => {//添加定时器
             this.setState({
                 num:++timerSj
             })
-            if (this.state.num === ssj) { //判断歌曲是否唱完
+
+            paowu.style.left=this.state.num*this.state.jdw+"px";//进度条动起来
+            jzcd.style.width=this.state.num*this.state.jdw+"px";//已放的长度
+            if (this.state.num === this.state.ssjj&&paowu.offsetLeft>=musicjd.offsetWidth) { //判断歌曲是否唱完，进度条是否完成
                 this.setState({
                     index: ++this.props.location.state.index,
-                    num:0
+                    num:0,
+                    jdw:0
                 })
+                jzcd.style.width=0//进度为0
+                paowu.style.left=0//让偏移量重新为0
+
                 clearInterval(this.state.timer)//关闭音频定时器
                 this.timerMusic(this.state.num)//开启定时器
                 this.playMusic(this.state.urlList[this.props.location.state.index].mainSong.id)
